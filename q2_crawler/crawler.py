942ccbd58048009d1af7cfc2f9efe09696ec0607
AT073198 <AT073198@AIA.TW>
2019-06-27 16:13:17 +0800

    Update get_disease_by_specialty().

--- q2_crawler/crawler.py
+++ q2_crawler/crawler.py
@@ -7,7 +7,6 @@ def get_disease_by_specialty(specialty):
     url = 'http://www.a-hospital.com/w/%E7%96%BE%E7%97%85'
     response = requests.get(url)
     response.encoding = 'utf-8'
-
     soup = BeautifulSoup(response.text, 'lxml')
 
     result = {}
@@ -19,7 +18,7 @@ def get_disease_by_specialty(specialty):
 
         result[specialty] = {}
 
-        sub_specialty = ''
+        sub_specialty = None
         current_tag = target.find_parent('h3')
 
         while current_tag.next_sibling.name != 'h3':
@@ -32,6 +31,12 @@ def get_disease_by_specialty(specialty):
                 result[specialty][sub_specialty] = []
 
             if current_tag.name == 'p':
+
+                if sub_specialty is None:
+                    sub_specialty = specialty
+                    if sub_specialty not in result[specialty]:
+                        result[specialty][sub_specialty] = []
+
                 # diseases
                 diseases = current_tag.find_all('a')
                 for d in diseases:
@@ -46,3 +51,15 @@ if __name__== "__main__":
 
     with open('internal_medicine.json', 'w', encoding = 'utf8') as json_file:
         json.dump(internal_medicine, json_file, ensure_ascii = False)
+
+    # get all diseases under '外科疾病'
+    # surgery = get_disease_by_specialty('外科疾病')
+    #
+    # with open('surgery.json', 'w', encoding = 'utf8') as json_file:
+    #     json.dump(surgery, json_file, ensure_ascii = False)
+
+    # get all diseases under '儿科疾病'
+    # pediatrics = get_disease_by_specialty('儿科疾病')
+    #
+    # with open('pediatrics.json', 'w', encoding = 'utf8') as json_file:
+    #     json.dump(pediatrics, json_file, ensure_ascii = False)
